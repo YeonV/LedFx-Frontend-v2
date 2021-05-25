f3fbfc98a4e52bd2cb81b5af3e8f418b57532aad
YeonV <trusted.yeon@gmail.com>
2021-05-25 21:30:16 +0200

    add Devices, Integrations, Scenes and Settings

--- src/pages/Devices.js
+++ src/pages/Devices.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useStore from "../utils/Api";
 
 const Devices = () => {
-  return <div>Devices</div>;
+  const getDisplays = useStore((state) => state.getDisplays);
+  const displays = useStore((state) => state.displays);
+  useEffect(() => {
+    getDisplays();
+  }, [getDisplays]);
+  return (
+    <div>
+      <h1>Devices</h1>
+      {displays && Object.keys(displays).map((s, i) => <div key={i}>{s}</div>)}
+    </div>
+  );
 };
 
 export default Devices;
--- src/pages/Scenes.js
+++ src/pages/Scenes.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useStore from "../utils/Api";
 
 const Scenes = () => {
-  return <div>Scenes</div>;
+  const getScenes = useStore((state) => state.getScenes);
+  const scenes = useStore((state) => state.scenes);
+  useEffect(() => {
+    getScenes();
+  }, [getScenes]);
+  return (
+    <div>
+      <h1>Scenes</h1>
+      {scenes && Object.keys(scenes).map((s, i) => <div key={i}>{s}</div>)}
+    </div>
+  );
 };
 
 export default Scenes;
