42f0c6ca66274db600270d090e5fee7f63be8fc5
YeonV <trusted.yeon@gmail.com>
2021-05-31 14:28:46 +0200

    cleanup

--- src/pages/Settings/AudioInputs.jsx
+++ src/pages/Settings/AudioInputs.jsx
@@ -1,14 +1,10 @@
 import {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import useStore from '../../utils/apiStore';
-
 
 const useStyles = makeStyles({
     form: {
@@ -17,28 +13,24 @@ const useStyles = makeStyles({
 });
 
 const AudioCard = ({  }) => {
-    const classes = useStyles();
-    
+    const classes = useStyles();    
     const getAudioInputs = useStore((state) => state.getAudioInputs)
     const setAudioInput = useStore((state) => state.setAudioInput)
     const devices = useStore((state) => state.settings && state.settings.audio_inputs && state.settings.audio_inputs.devices)
     const active_device_index = useStore((state)=>state.settings && state.settings.audio_inputs && state.settings.audio_inputs.active_device_index)
-    const handleAudioSelected = e => setAudioInput(e.target.value).then(()=>getAudioInputs());
     
     useEffect(() => {
       getAudioInputs();      
     }, [getAudioInputs]);
 
-
-    return devices ?
-      (Object.entries(devices).length > 0 ? (
+    return (Object.entries(devices).length > 0 ? (
         <FormControl className={classes.form} >
             <InputLabel id="audio-input-select-label">Audio Input Device</InputLabel>
             <Select
                 labelId="audio-input-select-label"
                 id="audio-input-select"
                 value={active_device_index}
-                onChange={(e) => handleAudioSelected(e)}
+                onChange={(e) => setAudioInput(e.target.value).then(()=>getAudioInputs())}
             >
                 {Object.entries(devices).map((e,i)=>
                     <MenuItem key={i} value={e[0]}>
@@ -47,28 +39,7 @@ const AudioCard = ({  }) => {
                 )}
             </Select>
         </FormControl>
-    ) : (
-            <Card>
-                <CardHeader title="Audio Input" subheader="Select an audio source for LedFx" />
-                <CardContent>
-                    <FormControl  className={classes.form}>
-                        <InputLabel id="audio-input-select-label">Current Device</InputLabel>
-                        <Select
-                            labelId="audio-input-select-label"
-                            id="audio-input-select"
-                            value={active_device_index}
-                            onChange={(e)=>handleAudioSelected(e)}
-                        >
-                          {Object.entries(devices).map((e,i)=>
-                              <MenuItem key={i} value={e[0]}>
-                                  {e[1]}{e[1].length > 30 && '...' }
-                              </MenuItem>
-                          )}
-                        </Select>
-                    </FormControl>
-                </CardContent>
-            </Card>
-        )) : (<></>);
+    )  : (<></>))
 };
 
 export default AudioCard;
