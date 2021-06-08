f77d86abb148e07e284732688f9bea2783ce9fdc
YeonV <trusted.yeon@gmail.com>
2021-06-08 10:15:59 +0200

    EditVirtuals

--- src/pages/Devices/EditVirtuals.jsx
+++ src/pages/Devices/EditVirtuals.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -8,17 +8,13 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Slide from '@material-ui/core/Slide';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import SaveIcon from '@material-ui/icons/Save';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 
 import { getOverlapping } from "../../utils/helpers";
 import useStore from '../../utils/apiStore';
 
 import AddSegmentDialog from './_AddSegmentDialog';
-import Segment from './_Segment';
-
-// import { updateDisplayConfig } from 'modules/displays';
-// import { showdynSnackbar } from 'modules/ui';
+import Segment from './Segment';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -49,39 +45,30 @@ export default function FullScreenDialog({
 }) {
   const classes = useStyles();
   const showSnackbar = useStore((state) => state.showSnackbar);
-  const updateDisplaySegments = useStore((state) => state.updateDisplaySegments);
-  
   const getDevices = useStore((state) => state.getDevices);
-  const devices = useStore((state) => state.devices);
-
   const [open, setOpen] = React.useState(false);
+  
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleClose = () => {   
+    const output = getOverlapping(display.segments);
+    const overlap = Object.keys(output).find(k => output[k].overlap);
+    if (overlap) {
+      showSnackbar({
+        message: `Overlapping in ${overlap} detected! Please Check your config`,
+        messageType: 'warning'
+      })
+    } else {
+      setOpen(false);
+    }
   };
 
-  // console.log(devices)
+
   useEffect(() => {
     getDevices()
   }, [getDevices])
- 
-
-  const handleSave = () => {
-    const output = getOverlapping(display.segments);
-    const overlap = Object.keys(output).find(k => output[k].overlap);
-
-    if (overlap) {
-        showSnackbar({
-          message: `Overlapping in ${overlap} detected! Please Check your config`,
-        })
-    } else {
-      updateDisplaySegments({ displayId: display.id, segments: display.segments });
-      // setOpen(false);
-    }
-  };
 
   return display && display.config ? (
     <>
@@ -110,17 +97,6 @@ export default function FullScreenDialog({
             <Typography variant="h6" className={classes.title}>
               {display.config.name}{' '}
             </Typography>
-            <div>
-              <Button
-                autoFocus
-                color="primary"
-                variant="contained"
-                endIcon={<SaveIcon />}
-                onClick={handleSave}
-              >
-                save
-                            </Button>
-            </div>
           </Toolbar>
         </AppBar>
 
@@ -138,7 +114,7 @@ export default function FullScreenDialog({
         </div>
         {display.segments.length > 0 &&
           display.segments.map((s, i) => (
-            <Segment s={s} i={i} key={i} display={display} />
+            <Segment s={s} i={i} key={i} display={display} segments={display.segments} />
           ))}
         <div
           style={{
@@ -153,5 +129,5 @@ export default function FullScreenDialog({
         </div>
       </Dialog>
     </>
-  ):(<></>);
+  ) : (<></>);
 }
