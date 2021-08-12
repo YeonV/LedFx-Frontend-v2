0fdceb0ec35553217247a79d9178d40295f85054
Yeon Vinzenz Varapragasam <trusted.yeon@gmail.com>
2021-08-12 17:02:51 +0200

    LongPress; PresetsCard

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CloseIcon from '@material-ui/icons/Close';
 import CheckIcon from '@material-ui/icons/Check';
+import { useLongPress } from 'use-long-press';
 
 const useStyles = makeStyles((theme) => ({
   typography: {
@@ -15,6 +16,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Popover({
   onConfirm,
+  onSingleClick,
+  onDoubleClick,
+  onLongPress,
+  openOnDoubleClick = false,
+  openOnLongPress = false,
   variant = 'contained',
   color = 'secondary',
   label,
@@ -25,36 +31,68 @@ export default function Popover({
   icon = <DeleteIcon />,
   className,
   startIcon,
+  noIcon = false,
   style = {},
-  disabled=false,
+  disabled = false,
 }) {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const openPopover = (event) => {
+    setAnchorEl(event.currentTarget || event.target)
   };
   const handleClose = () => {
-    setAnchorEl(null);
+    setAnchorEl(null)
   };
+  const longPress = useLongPress((e) => openPopover(e), {
+    onCancel: e => {
+      console.log('click')
+      onSingleClick && onSingleClick()
+    },
+    treshhold: 1000,  
+    captureEvent: true,
+  });
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
   return (
     <div style={{ display: 'initial' }}>
-      <Button
-        aria-describedby={id}
-        variant={variant}
-        color={color}
-        onClick={handleClick}
-        size={size}
-        className={className}
-        style={style}
-        startIcon={startIcon}
-        disabled={disabled}
-      >
-        {label}
-        {!startIcon && icon}
-      </Button>
+      {openOnLongPress
+        ? <Button
+          aria-describedby={id}
+          variant={variant}
+          color={color}
+          size={size}
+          className={className}
+          style={style}
+          startIcon={!noIcon && startIcon}
+          disabled={disabled}
+          {...longPress}
+        >
+          {label}
+          {!startIcon && !noIcon && icon}
+        </Button>
+        : <Button
+          aria-describedby={id}
+          variant={variant}
+          color={color}
+          onClick={(e) => {
+            console.log("click")
+            !openOnDoubleClick && openPopover(e)
+            onSingleClick && onSingleClick(e)
+          }}
+          size={size}
+          className={className}
+          style={style}
+          startIcon={!noIcon && startIcon}
+          disabled={disabled}
+          onDoubleClick={(e) => {
+            openOnDoubleClick && openPopover(e)
+            onDoubleClick && onDoubleClick(e)
+          }}
+        >
+          {label}
+          {!startIcon && !noIcon && icon}
+        </Button>}
       <PopoverOriginal
         id={id}
         open={open}
