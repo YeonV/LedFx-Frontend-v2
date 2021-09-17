6437b2bd139d5b5da4af9eab5cd752020d9bff35
Yeon Vinzenz Varapragasam <trusted.yeon@gmail.com>
2021-09-17 17:57:09 +0200

    Extend style attribute on Icon

--- src/components/Icons/BladeIcon.jsx
+++ src/components/Icons/BladeIcon.jsx
@@ -7,6 +7,7 @@ const BladeIcon = ({
   colorIndicator = false,
   name = "MusicNote",
   className,
+  style,
   scene = false,
   card = false
 }) => {
@@ -14,13 +15,13 @@ const BladeIcon = ({
     <Icon
       className={className}
       color={colorIndicator ? 'primary' : 'inherit'}
-      style={{ position: 'relative' }}
+      style={style}
     >
-      {name.startsWith('yz:logo2y') ? <YZLogo2Y style={{ transform: card ? 'scale(0.025)' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
-        : name.startsWith('yz:logo2z') ? <YZLogo2Z style={{ transform: card ? 'scale(0.025)' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
-        : name.startsWith('yz:logo2top') ? <YZLogo2Top style={{ transform: card ? 'scale(0.025)' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
-        : name.startsWith('yz:logo2bot') ? <YZLogo2Bottom style={{ transform: card ? 'scale(0.025)' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
-        : name.startsWith('yz:logo2') ? <YZLogo2 style={{ transform: card ? 'scale(0.025)' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
+      {name.startsWith('yz:logo2y') ? <YZLogo2Y style={{ transform: card ? 'unset' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
+        : name.startsWith('yz:logo2z') ? <YZLogo2Z style={{ transform: card ? 'unset' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
+        : name.startsWith('yz:logo2top') ? <YZLogo2Top style={{ transform: card ? 'unset' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
+        : name.startsWith('yz:logo2bot') ? <YZLogo2Bottom style={{ transform: card ? 'unset' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
+        : name.startsWith('yz:logo2') ? <YZLogo2 style={{ transform: card ? 'unset' : scene ? 'scale(1)' : 'scale(0.012)', marginTop: '3px' }} />
         : name.startsWith('wled') ? <Wled />
         : name.startsWith('mdi:') ? <span className={`mdi mdi-${name.split('mdi:')[1]}`} />
         : camelToSnake(name)}
