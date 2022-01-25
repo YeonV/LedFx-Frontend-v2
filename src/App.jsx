4f3c96b4512abb3cfe1fa953ed00a769652381e0
YeonV <trusted.yeon@gmail.com>
2022-01-25 02:35:19 +0100

    Add to HomeScreen

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,23 @@ import clsx from 'clsx';
 import { CssBaseline } from '@material-ui/core';
 import useStore from './utils/apiStore';
 import ScrollToTop from './utils/scrollToTop';
-import ws, { WsContext, HandleWs } from "./utils/Websocket";
-import { BladeDarkTheme, BladeDarkOrangeTheme, BladeLightTheme, BladeDarkGreenTheme, BladeDarkBlueTheme, BladeDarkGreyTheme } from './AppThemes'
-import { BladeDarkTheme5, BladeDarkOrangeTheme5, BladeLightTheme5, BladeDarkGreenTheme5, BladeDarkBlueTheme5, BladeDarkGreyTheme5 } from './AppThemes5'
+import ws, { WsContext, HandleWs } from './utils/Websocket';
+import {
+  BladeDarkTheme,
+  BladeDarkOrangeTheme,
+  BladeLightTheme,
+  BladeDarkGreenTheme,
+  BladeDarkBlueTheme,
+  BladeDarkGreyTheme,
+} from './AppThemes';
+import {
+  BladeDarkTheme5,
+  BladeDarkOrangeTheme5,
+  BladeLightTheme5,
+  BladeDarkGreenTheme5,
+  BladeDarkBlueTheme5,
+  BladeDarkGreyTheme5,
+} from './AppThemes5';
 import useStyles from './App.styles';
 import './App.css';
 
@@ -28,7 +42,7 @@ import LoginRedirect from './pages/LoginRedirect';
 import isElectron from 'is-electron';
 import WaveLines from './components/Icons/waves';
 import useWindowDimensions from './utils/useWindowDimension';
-import { useHotkeys } from 'react-hotkeys-hook'
+import { useHotkeys } from 'react-hotkeys-hook';
 import SmartBar from './components/Dialogs/SmartBar';
 import wsNew, { HandleWsNew, WsContextNew } from './utils/WebsocketNew';
 import { TrainRounded } from '@mui/icons-material';
@@ -36,12 +50,13 @@ import { SnackbarProvider } from 'notistack';
 import { Close } from '@material-ui/icons';
 import { useSnackbar } from 'notistack';
 import AddToHomeScreen from '@ideasio/add-to-homescreen-react';
-
-
+import './AddToHomescreen.css';
 
 export default function App() {
   const classes = useStyles();
-  const leftBarOpen = useStore((state) => state.ui.bars && state.ui.bars.leftBar.open);
+  const leftBarOpen = useStore(
+    (state) => state.ui.bars && state.ui.bars.leftBar.open
+  );
   const getVirtuals = useStore((state) => state.getVirtuals);
   const getSystemConfig = useStore((state) => state.getSystemConfig);
   const getSchemas = useStore((state) => state.getSchemas);
@@ -53,35 +68,38 @@ export default function App() {
   const showSnackbar = useStore((state) => state.showSnackbar);
   const setHost = useStore((state) => state.setHost);
 
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   useHotkeys('ctrl+alt+y', () => setOpen(!open));
 
   const { height, width } = useWindowDimensions();
 
-  let newBase = !!window.localStorage.getItem('ledfx-newbase')
+  let newBase = !!window.localStorage.getItem('ledfx-newbase');
 
-  if (window.location.hash.indexOf("newCore=1") > -1) {
-    window.localStorage.setItem('ledfx-newbase', 1)
-    window.localStorage.setItem('ledfx-host', 'http://localhost:8080')
-    newBase = 1
+  if (window.location.hash.indexOf('newCore=1') > -1) {
+    window.localStorage.setItem('ledfx-newbase', 1);
+    window.localStorage.setItem('ledfx-host', 'http://localhost:8080');
+    newBase = 1;
   }
 
-  const ledfxTheme = !!window.localStorage.getItem('ledfx-theme') ?
-    window.localStorage.getItem('ledfx-theme')
-    : !!window.localStorage.getItem('hassTokens') ? 'DarkBlue'
-      : window.location.origin === 'https://my.ledfx.app' ? 'DarkGreen'
-        : isElectron() ? 'DarkOrange'
-          : 'DarkRed';
+  const ledfxTheme = !!window.localStorage.getItem('ledfx-theme')
+    ? window.localStorage.getItem('ledfx-theme')
+    : !!window.localStorage.getItem('hassTokens')
+    ? 'DarkBlue'
+    : window.location.origin === 'https://my.ledfx.app'
+    ? 'DarkGreen'
+    : isElectron()
+    ? 'DarkOrange'
+    : 'DarkRed';
 
   const ledfxThemes = {
-    "Dark": BladeDarkTheme,
-    "DarkRed": BladeDarkTheme,
-    "DarkOrange": BladeDarkOrangeTheme,
-    "Light": BladeLightTheme,
-    "DarkGreen": BladeDarkGreenTheme,
-    "DarkBlue": BladeDarkBlueTheme,
-    "DarkGrey": BladeDarkGreyTheme
-  }
+    Dark: BladeDarkTheme,
+    DarkRed: BladeDarkTheme,
+    DarkOrange: BladeDarkOrangeTheme,
+    Light: BladeLightTheme,
+    DarkGreen: BladeDarkGreenTheme,
+    DarkBlue: BladeDarkBlueTheme,
+    DarkGrey: BladeDarkGreyTheme,
+  };
 
   useEffect(() => {
     getVirtuals();
@@ -89,42 +107,47 @@ export default function App() {
     getSchemas();
   }, [getVirtuals, getSystemConfig, getSchemas]);
 
-
   useEffect(() => {
-    if (features['go'] || window.location.hash.indexOf("newCore=1") > -1) {
-      window.localStorage.setItem('ledfx-newbase', 1)
-      window.localStorage.removeItem('undefined')
-      window.localStorage.removeItem('ledfx-hosts')
+    if (features['go'] || window.location.hash.indexOf('newCore=1') > -1) {
+      window.localStorage.setItem('ledfx-newbase', 1);
+      window.localStorage.removeItem('undefined');
+      window.localStorage.removeItem('ledfx-hosts');
       setShowFeatures('go', true);
       setFeatures('go', true);
-      if (window.localStorage.getItem('ledfx-host') !== 'http://localhost:8080') {
-        window.localStorage.setItem('ledfx-host', 'http://localhost:8080')
+      if (
+        window.localStorage.getItem('ledfx-host') !== 'http://localhost:8080'
+      ) {
+        window.localStorage.setItem('ledfx-host', 'http://localhost:8080');
       }
-      setHost('http://localhost:8080')
-      newBase = true
+      setHost('http://localhost:8080');
+      newBase = true;
     } else {
-      window.localStorage.removeItem('ledfx-newbase')
-      newBase = false
+      window.localStorage.removeItem('ledfx-newbase');
+      newBase = false;
     }
   }, []);
 
   useEffect(() => {
     initFrontendConfig();
-    console.info('%c Ledfx ' + '%c\n ReactApp by Blade ', 'padding: 10px 40px; color: #ffffff; border-radius: 5px 5px 0 0; background-image:url(https://my.ledfx.app/favicon/favicon-32x32.png); background-color: #800000; background-repeat: no-repeat; background-position: 10% 50%;', 'background: #fff; color: #800000; border-radius: 0 0 5px 5px;padding: 5px 0;');
+    console.info(
+      '%c Ledfx ' + '%c\n ReactApp by Blade ',
+      'padding: 10px 40px; color: #ffffff; border-radius: 5px 5px 0 0; background-image:url(https://my.ledfx.app/favicon/favicon-32x32.png); background-color: #800000; background-repeat: no-repeat; background-position: 10% 50%;',
+      'background: #fff; color: #800000; border-radius: 0 0 5px 5px;padding: 5px 0;'
+    );
     // log("successSUCCESS", { id: 'test' })
     // log("warningWARNING", { id: 'test' })
     // log("infoINFO", { id: 'test' })
-    if (features['go'] || window.location.hash.indexOf("newCore=1") > -1) {
-      window.localStorage.setItem('ledfx-host', 'http://localhost:8080')
+    if (features['go'] || window.location.hash.indexOf('newCore=1') > -1) {
+      window.localStorage.setItem('ledfx-host', 'http://localhost:8080');
     }
   }, []);
 
   window.api?.receive('fromMain', (parameters) => {
     if (parameters === 'shutdown') {
-      shutdown()
+      shutdown();
     }
     if (parameters === 'clear-frontend') {
-      deleteFrontendConfig()
+      deleteFrontendConfig();
     }
   });
 
@@ -134,13 +157,12 @@ export default function App() {
         message: e.detail.message,
         messageType: e.detail.type,
       });
-    }
-    document.addEventListener("YZNEW", handleWebsockets);
+    };
+    document.addEventListener('YZNEW', handleWebsockets);
     return () => {
-      document.removeEventListener("YZNEW", handleWebsockets)
-    }
+      document.removeEventListener('YZNEW', handleWebsockets);
+    };
   }, []);
-
 
   return (
     <ThemeProvider theme={BladeDarkGreyTheme5}>
@@ -148,8 +170,10 @@ export default function App() {
         <SnackbarProvider maxSnack={5}>
           <WsContextNew.Provider value={wsNew}>
             <WsContext.Provider value={ws}>
-              <div className={classes.root} style={{ paddingTop: isElectron() ? '30px' : 0 }}>
-                <AddToHomeScreen activateLogging />
+              <div
+                className={classes.root}
+                style={{ paddingTop: isElectron() ? '30px' : 0 }}
+              >
                 <CssBaseline />
                 <Router basename={process.env.PUBLIC_URL}>
                   <ScrollToTop />
@@ -165,7 +189,11 @@ export default function App() {
                   >
                     <div className={classes.drawerHeader} />
                     <Switch>
-                      <Route exact path="/connect/:providerName/redirect" component={LoginRedirect} />
+                      <Route
+                        exact
+                        path="/connect/:providerName/redirect"
+                        component={LoginRedirect}
+                      />
                       <Route exact path="/" component={Home} />
                       <Route path="/devices" component={Devices} />
                       <Route path="/device/:virtId" component={Device} />
@@ -178,15 +206,61 @@ export default function App() {
                   </main>
                   <BottomBar />
                 </Router>
-                {features['waves'] && <WaveLines
-                  startColor={ledfxThemes[ledfxTheme || 'DarkRed'].palette.primary.main}
-                  stopColor={ledfxThemes[ledfxTheme || 'DarkRed'].palette.accent.main || '#ffdc0f'}
-                  width={width - 8}
-                  height={height}
-                />}
+                {features['waves'] && (
+                  <WaveLines
+                    startColor={
+                      ledfxThemes[ledfxTheme || 'DarkRed'].palette.primary.main
+                    }
+                    stopColor={
+                      ledfxThemes[ledfxTheme || 'DarkRed'].palette.accent
+                        .main || '#ffdc0f'
+                    }
+                    width={width - 8}
+                    height={height}
+                  />
+                )}
               </div>
             </WsContext.Provider>
           </WsContextNew.Provider>
+          <AddToHomeScreen
+            // debug={true}
+            // activateLogging={true}
+            startAutomatically={false}
+            appId="Add LedFx-to-Homescreen React Live Demo"
+            skipFirstVisit={false}
+            displayPace={0}
+            lifespan={60}
+            // maxDisplayCount={5}
+            customPromptContent={{
+              title: 'Do you want to add LedFx to your homescreen?',
+              cancelMsg: 'X',
+              installMsg: 'Add',
+              guidanceCancelMsg: 'X',
+              src: '/favicon/apple-icon-76x76.png',
+            }}
+            customPromptElements={{
+              container: 'athContainer',
+              containerAddOns: '',
+              banner: 'athBanner',
+              logoCell: 'athLogoCell',
+              logoCellAddOns: 'athContentCell',
+              logo: 'athLogo',
+              titleCell: 'athTitleCell',
+              titleCellAddOns: 'athContentCell',
+              title: 'athTitle',
+              cancelButtonCell: 'athCancelButtonCell',
+              cancelButtonCellAddOns: 'athButtonCell',
+              cancelButton: 'athCancelButton',
+              installButtonCell: 'athInstallButtonCell',
+              installButtonCellAddOns: 'athButtonCell',
+              installButton: 'athInstallButton',
+              installButtonAddOns: 'button',
+              guidance: 'athGuidance',
+              guidanceImageCell: 'athGuidanceImageCell',
+              guidanceImageCellAddOns: '',
+              guidanceCancelButton: 'athGuidanceCancelButton'
+            }}
+          />
         </SnackbarProvider>
       </MuiThemeProvider>
     </ThemeProvider>
