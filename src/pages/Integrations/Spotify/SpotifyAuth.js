6ebfb5eeab1e5c3b28a61bf0f42f44bbe5b4d7d4
Mattallmighty <matt4788667@hotmail.com>
2022-03-12 13:20:02 +1100

    .

--- src/pages/Integrations/Spotify/SpotifyAuth.js
+++ src/pages/Integrations/Spotify/SpotifyAuth.js
@@ -1,6 +1,7 @@
 import useStore from '../../../utils/apiStore';
 import Button from '@material-ui/core/Button';
 import { Grid, Typography } from '@material-ui/core';
+import request from "./axiosClient";
 
   const productionUrl = "https://my.ledfx.app/#/./Integrations";
   const localUrl = "http://localhost:3000/#/./Integrations";
@@ -125,5 +126,55 @@ const SpotifyView = () => {
         </Grid>
   );
 };
+
+const setTokenSelector = (state) => state.setToken;
+const setIsAuthenticatedSelector = (state) => state.setIsAuthenticated;
+
+export function useLogin() {
+  const setToken = useAuthStore(setTokenSelector);
+  const setIsAuthenticated = useAuthStore(setIsAuthenticatedSelector);
+
+  useLayoutEffect(() => {
+    const token = getTokenFromUrl();
+    if (token) {
+      setAxiosHeaders(token);
+      setToken(token);
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+      removeAxiosAuthHeader();
+    }
+  }, [setToken, setIsAuthenticated]);
+}
+
+export const getTokenFromUrl = () => {
+    const hashParams = {};
+    let e = "";
+    const r = /([^&;=]+)=?([^&;]*)/g;
+    const q = window.location.hash.substring(1);
+    // eslint-disable-next-line no-cond-assign
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+  
+    if (hashParams.access_token) {
+      return hashParams.access_token;
+    }
+    return false;
+  };
+  
+  export const isTokenValid = (date) => {
+    const HOUR = 1000 * 60 * 60;
+    const anHourAgo = Date.now() - HOUR;
+    return date > anHourAgo;
+  };
+  
+  export const setAxiosHeaders = (token) => {
+    request.defaults.headers.common.Authorization = `Bearer ${token}`;
+  };
+  
+  export const removeAxiosAuthHeader = () => {
+    delete request.defaults.headers.common.Authorization;
+  };
 
 export default SpotifyView;
