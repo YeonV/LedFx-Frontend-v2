ea40b5ca519cbfa6117ee796151c4514a60c9c33
Mattallmighty <matt4788667@hotmail.com>
2022-03-28 12:46:37 +1100

    .

--- src/pages/Integrations/Spotify/SpotifyAuth.js
+++ src/pages/Integrations/Spotify/SpotifyAuth.js
@@ -1,10 +1,16 @@
+import React,{useEffect} from 'react'
 import Button from '@material-ui/core/Button';
 import { Grid, Typography } from '@material-ui/core';
 import { useState } from 'react';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
 import axios from 'axios';
+import Cookies from 'universal-cookie/es6'
+import { finishAuth,refreshAuth,logoutAuth } from './proxies';
+import getPkce from 'oauth-pkce';
+import useStore from '../../../utils/apiStore';
 
-const setIsAuthenticated = false;
+
+
 const productionUrl = 'https://my.ledfx.app/callback/#/Integrations';
 const localUrl = 'http://localhost:3000/callback/#/Integrations?';
 const SpotifyRedirectURL = axios.create({
@@ -35,46 +41,97 @@ const apiCredentials = {
     'user-library-modify',
   ],
 };
-const SpotifyURL = `https://accounts.spotify.com/authorize?client_id=${
-  apiCredentials.CLIENT_ID
-}&redirect_uri=${encodeURIComponent(
-  apiCredentials.REDIRECT_URL
-)}&scope=${encodeURIComponent(
-  apiCredentials.SCOPES.join(' ')
-)}&response_type=token`;
-0
+
 export function SpotifyLoginRedirect(props) {
-  const [text, setText] = useState('Loading...');
-  const location = useLocation();
-  const params = useParams();
-  const history = useNavigate();
-  let expDate = new Date();
-  expDate.setHours(expDate.getHours() + 1);
-  console.log("Spotify Token", location.hash.split('token=')[1])
-  localStorage.setItem('Spotify-Token', location.hash.split('token=')[1]);
+  const search = window.location.search.substring(1);
+    const params = JSON.parse(
+        '{"' +
+            decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') +
+            '"}'
+    );
+
+  localStorage.setItem('Spotify-Token', params.code);
+  
   setTimeout(() => window.location.href = 'http://localhost:3000/#/Integrations', 200); // Redirect to homepage after 3 sec
-  return <p>{text}</p>;
+  return <p>{params.code}</p>;
 };
-
-export function logoutClick(props) {
-  localStorage.removeItem('Spotify-Token');
-  window.location.href = 'http://localhost:3000/#/Integrations';
-  setIsAuthenticated = false;
-  isAuthenticated = false;
-};
-
-//Need to figure out how to put in Spoitfy token exiry time.
- const SpotifyAuthdValid = (props) => {
-  let token = localStorage.getItem('Spotify-Token');
-  if (localStorage.getItem('Spotify-Token') !== null) {
-    setIsAuthenticated = true}
-    else {setIsAuthenticated = false}
- }
 
 const SpotifyView = (props) => {
   //SpotifyAuthdValid();
-  SpotifyAuthdValid();
-  console.log("Is Spotify Auth'd?", setIsAuthenticated)
+
+  const [isAuthenticated,setIsAuthenticated]=React.useState(false);
+  const setSpotifyAuthToken = useStore((state) => state.setSpotifyAuthToken);
+  const [ codes, setCodes]  = React.useState({})
+  const cookies = new Cookies();
+
+
+  React.useEffect(()=>{
+    getPkce(50, (error, { verifier, challenge }) => {
+      setCodes({ verifier, challenge });
+    });
+    console.log(codes)
+    if(cookies.get('access_token')){
+      setIsAuthenticated(true);
+    }
+  },[])
+  const beginAuth = () => {
+    
+    cookies.set('verifier', codes.verifier);
+    let authURL =
+        `https://accounts.spotify.com/authorize/` +
+        '?response_type=code' +
+        '&client_id=' +
+        encodeURIComponent('7658827aea6f47f98c8de593f1491da5') +
+        '&scope=' +
+        encodeURIComponent(
+            'user-library-read user-library-modify user-read-email user-top-read streaming user-read-private user-read-playback-state user-modify-playback-state'
+        ) +
+        '&redirect_uri=' +
+        encodeURIComponent(apiCredentials.REDIRECT_URL) +
+        '&code_challenge=' +
+        encodeURIComponent(codes.challenge) +
+        '&code_challenge_method=S256';
+    console.log(authURL);
+    window.location.href = authURL;
+};
+
+useEffect(() => {
+  const accessTest = cookies.get('logout');
+  const accessTest1 = cookies.get('access_token');
+  if ((accessTest === 'false' || !accessTest) && !accessTest1) {
+      // dispatch(refreshAuth());
+      refreshAuth();
+      cookies.set('logout', false);
+      setIsAuthenticated(true);
+  }
+  console.log('Got to useEffect:', window.location.search);
+  if (localStorage.getItem('Spotify-Token')) {
+      console.log('Got to here');
+      // dispatch(finishAuth());
+      setIsAuthenticated(true);
+
+      try{
+      finishAuth()
+
+    }catch(err){
+        console.log(err)
+      }
+  }
+  // dispatch(editAsyncIntegration());
+  // dispatch(checkCookiesForTokens());
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+
+useEffect(()=>{
+if(cookies.get('access_token')){
+  setIsAuthenticated(true);
+  setSpotifyAuthToken(cookies.get('access_token'));
+}else{
+  setIsAuthenticated(false);
+
+}
+},[cookies])
+
   return (
     <Grid
       container
@@ -82,22 +139,27 @@ const SpotifyView = (props) => {
       alignContent="center"
       //style={{ height: '50%' }}
     >
-      {!setIsAuthenticated ? (
+      {!isAuthenticated ? (
         <Button
           variant="contained"
           color="primary"
           onClick={(e) => {
             console.log("AND IT BEGINS....")            
-            window.location.href = SpotifyURL;
+           beginAuth();
           }}
         >
           <Typography>Connect to Spotify</Typography>
         </Button>
-      ) : setIsAuthenticated ? (
+      ) : isAuthenticated ? (
         <Button
           variant="contained"
           color="primary"
-          onClick={() => logoutClick()}
+          onClick={() => {logoutAuth();
+  setIsAuthenticated(false);
+
+      }
+          
+          }
         >
           <Typography>Logout</Typography>
         </Button>
