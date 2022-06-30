aad21ceee923e87050681d6bc542a0c43794982d
YeonV <trusted.yeon@gmail.com>
2022-06-30 11:49:55 +0200

    .

--- public/renderer.ts
+++ public/renderer.ts
@@ -1,13 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
+// `nodeIntegration` is turned off. Use `preload.ts` to
 // selectively enable features needed in the rendering
 // process.
-// import { Titlebar } from 'custom-electron-titlebar'
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   new Titlebar({
-//       backgroundColor: '#800000'
-//   })
-// })
