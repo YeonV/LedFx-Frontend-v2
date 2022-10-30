c988b90ee1ef5ca47cda4a3c917591082dfab8b0
Mattallmighty <matt4788667@hotmail.com>
2022-10-31 09:51:45 +1100

    Fix if blank URL

--- src/components/Dialogs/EditSceneDialog.tsx
+++ src/components/Dialogs/EditSceneDialog.tsx
@@ -31,7 +31,7 @@ const EditSceneDialog = () => {
 
   function isValidURL(string: string) {
     const res = string.match(
-      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g
+      /(?![\s\S])|\d(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g
     );
     return res !== null;
   }
