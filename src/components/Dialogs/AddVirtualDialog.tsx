6dec8dd43679021a6d08a3db5cb2367bef3ba94e
YeonV <trusted.yeon@gmail.com>
2022-11-18 03:14:04 +0100

    Auto-open Segment-Setup after adding a new Virtual

--- src/components/Dialogs/AddVirtualDialog.tsx
+++ src/components/Dialogs/AddVirtualDialog.tsx
@@ -12,14 +12,17 @@ import {
   Button,
   Link,
 } from '@mui/material';
+import { Tune } from '@mui/icons-material';
 import useStore from '../../store/useStore';
 import BladeSchemaForm from '../SchemaForm/SchemaForm/SchemaForm';
+import EditVirtuals from '../../pages/Devices/EditVirtuals/EditVirtuals';
 
 const AddVirtualDialog = () => {
   const addVirtual = useStore((state) => state.addVirtual);
   const getDevices = useStore((state) => state.getDevices);
   const getVirtuals = useStore((state) => state.getVirtuals);
   const virtuals = useStore((state) => state.virtuals);
+  const [expanded, setExpanded] = useState(false);
 
   const open = useStore((state) => state.dialogs.addVirtual?.open || false);
   const virtId = useStore((state) => state.dialogs.addVirtual?.edit || false);
@@ -38,6 +41,7 @@ const AddVirtualDialog = () => {
   const setDialogOpenAddVirtual = useStore(
     (state) => state.setDialogOpenAddVirtual
   );
+  const setCurrentVirtual = useStore((state) => state.setCurrentVirtual);
 
   const virtualsSchemas = useStore((state) => state.schemas?.virtuals);
   const showSnackbar = useStore((state) => state.ui.showSnackbar);
@@ -47,6 +51,7 @@ const AddVirtualDialog = () => {
 
   const handleClose = () => {
     setDialogOpenAddVirtual(false);
+    setCurrentVirtual(null);
     setModel({});
   };
   const handleAddVirtual = () => {
@@ -81,6 +86,7 @@ const AddVirtualDialog = () => {
           setDialogOpenAddVirtual(false);
           getDevices();
           getVirtuals();
+          setModel({});
         }
         // else {
         // }
@@ -96,6 +102,7 @@ const AddVirtualDialog = () => {
           setDialogOpenAddVirtual(false);
           getDevices();
           getVirtuals();
+          setModel({});
         }
         // else {
         // }
@@ -112,64 +119,79 @@ const AddVirtualDialog = () => {
   }, [virtId]);
 
   return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="form-dialog-title"
-    >
-      <DialogTitle id="form-dialog-title">
-        {initial.config &&
-        Object.keys(initial.config).length === 0 &&
-        initial.config.constructor === Object
-          ? 'Add Virtual Device'
-          : 'Settings'}
-      </DialogTitle>
-      <DialogContent>
-        <DialogContentText component="div">
-          With Virtuals, you can split devices into segments and merge several
-          segments over several devices into one Virtual-Device.
-          <div style={{ marginTop: '1rem', color: '#666' }}>
-            {' '}
-            Icons:{' '}
-            <Link
-              href="https://material-ui.com/components/material-icons/"
-              target="_blank"
-            >
-              MUI
-            </Link>
-            (ie: iconName) |{' '}
-            <Link href="https://materialdesignicons.com" target="_blank">
-              MDI
-            </Link>
-            (ie: mdi:icon-name)
-          </div>
-        </DialogContentText>
-
-        <BladeSchemaForm
-          schema={currentSchema}
-          model={
-            initial.config &&
-            Object.keys(initial.config).length === 0 &&
-            initial.config.constructor === Object
-              ? model
-              : { ...model, ...initial.config }
-          }
-          onModelChange={handleModelChange}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={handleAddVirtual} color="primary">
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">
           {initial.config &&
           Object.keys(initial.config).length === 0 &&
           initial.config.constructor === Object
-            ? 'Add'
-            : 'Save'}
-        </Button>
-      </DialogActions>
-    </Dialog>
+            ? 'Add Virtual Device'
+            : 'Settings'}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText component="div">
+            With Virtuals, you can split devices into segments and merge several
+            segments over several devices into one Virtual-Device.
+            <div style={{ marginTop: '1rem', color: '#666' }}>
+              {' '}
+              Icons:{' '}
+              <Link
+                href="https://material-ui.com/components/material-icons/"
+                target="_blank"
+              >
+                MUI
+              </Link>
+              (ie: iconName) |{' '}
+              <Link href="https://materialdesignicons.com" target="_blank">
+                MDI
+              </Link>
+              (ie: mdi:icon-name)
+            </div>
+          </DialogContentText>
+
+          <BladeSchemaForm
+            schema={currentSchema}
+            model={
+              initial.config &&
+              Object.keys(initial.config).length === 0 &&
+              initial.config.constructor === Object
+                ? model
+                : { ...model, ...initial.config }
+            }
+            onModelChange={handleModelChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleAddVirtual} color="primary">
+            {initial.config &&
+            Object.keys(initial.config).length === 0 &&
+            initial.config.constructor === Object
+              ? 'Add & Setup Segments'
+              : 'Save'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <EditVirtuals
+        label="Edit Virtual"
+        variant="text"
+        color="inherit"
+        virtId={virtId}
+        icon={undefined}
+        className={`step-devices-six-${expanded}`}
+        startIcon={<Tune />}
+        type="hidden"
+        innerKey={undefined}
+        onClick={() => setExpanded(false)}
+      />
+    </>
   );
 };
 
