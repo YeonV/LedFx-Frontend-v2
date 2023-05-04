7ddd66399fbc6c8791fc43e2c468e11ad5b5afa8
YeonV <trusted.yeon@gmail.com>
2023-05-04 23:01:19 +0200

    Release 2.0.68

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/indent */
 export const drawerWidth = 240
-export const frontendConfig = 8
+export const frontendConfig = 9
 
 export const formatTime = (dura: number) => {
   let seconds: string | number
