8a3ae543fe28a951f448b12f91acde36969867b1
Matt Muller <matt4788667@hotmail.com>
2023-07-07 13:19:32 +1000

    refactored

--- src/components/MidiInput.tsx
+++ src/components/MidiInput.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { useEffect } from 'react'
-import { WebMidi, Input, NoteMessageEvent } from 'webmidi'
+import { WebMidi, Input } from 'webmidi'
 import useStore from '../store/useStore'
 
 const MIDIListener = () => {
@@ -8,21 +8,10 @@ const MIDIListener = () => {
   const activateScene = useStore((state) => state.activateScene)
 
   useEffect(() => {
-    const handleMidiEvent = (input: Input, event: NoteMessageEvent) => {
+    const handleMidiEvent = (input: Input, event: any) => {
       const midiInput = `${input.name} Note: ${event.note.identifier} buttonNumber: ${event.note.number}`
-      const inputName = '2- Launchpad S 16'
-      const output = WebMidi.getOutputByName(inputName)
-
-      if (output) {
-        // Reset all LEDs to off
-        output.send([0xb0, 0x00, 0x00])
-      } else {
-        console.error('Output device not found')
-      }
-
       Object.keys(scenes).forEach((key) => {
-        const scene = scenes[key] as { scene_midiactivate: string }
-
+        const scene = scenes[key]
         if (midiInput === String(scene.scene_midiactivate)) {
           activateScene(key)
         }
@@ -30,19 +19,19 @@ const MIDIListener = () => {
     }
 
     const handleMidiInput = (input: Input) => {
-      input.addListener('noteon', (event: NoteMessageEvent) => {
+      input.addListener('noteon', (event: any) => {
         handleMidiEvent(input, event)
       })
     }
 
     WebMidi.enable({
-      callback: (err: Error) => {
+      callback: (err: any) => {
         if (err) {
           console.error('WebMidi could not be enabled:', err)
         } else {
           const { inputs } = WebMidi
           if (inputs.length > 0) {
-            inputs.forEach((input: Input) => {
+            inputs.forEach((input) => {
               handleMidiInput(input)
             })
           }
@@ -50,8 +39,8 @@ const MIDIListener = () => {
       },
     })
 
-    // WebSocket setup
     const webSocket = new WebSocket('ws://localhost:8888/api/websocket')
+
     webSocket.addEventListener('open', () => {
       const message = JSON.stringify({
         event_type: 'scene_activated',
@@ -67,10 +56,7 @@ const MIDIListener = () => {
         if (data.type === 'event' && data.event_type === 'scene_activated') {
           const { scene_id } = data
           Object.keys(scenes).forEach((key) => {
-            const scene = scenes[key] as {
-              name: any
-              scene_midiactivate: string
-            }
+            const scene = scenes[key]
             if (scene.name === scene_id) {
               const inputName =
                 scene.scene_midiactivate.split(' Note:')[0] ||
@@ -82,13 +68,9 @@ const MIDIListener = () => {
               const output = WebMidi.getOutputByName(inputName)
 
               if (output) {
-                // Reset all LEDs to off
                 output.send([0xb0, 0x00, 0x00])
 
-                // Set the LED of the pressed button to on
-                if (Number.isNaN(buttonNumber)) {
-                  // bla
-                } else {
+                if (!Number.isNaN(buttonNumber)) {
                   output.send([0x90, buttonNumber, 60])
                 }
               } else {
