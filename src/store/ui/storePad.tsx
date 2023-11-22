93aeffebd97e48e04596a2f6c6e5dae7fd265e2e
YeonV <trusted.yeon@gmail.com>
2023-11-22 12:20:59 +0100

    provide default mapping

--- src/store/ui/storePad.tsx
+++ src/store/ui/storePad.tsx
@@ -2,12 +2,34 @@
 import { produce } from 'immer'
 import type { IStore } from '../useStore'
 
+const defaultMapping = {
+  8: {
+    mode: 'command',
+    command: 'smartbar'
+  },
+  9: {
+    mode: 'command',
+    command: 'play/pause'
+  },
+  12: {
+    mode: 'command',
+    command: 'brightness-up'
+  },
+  13: {
+    mode: 'command',
+    command: 'brightness-down'
+  },
+  16: {
+    mode: 'command',
+    command: 'padscreen'
+  }
+}
 const storePad = (set: any) => ({
   mapping: {
-    0: {},
-    1: {},
-    2: {},
-    3: {}
+    0: defaultMapping,
+    1: defaultMapping,
+    2: defaultMapping,
+    3: defaultMapping
   } as any,
   setMapping: (mapping: any): void =>
     set(
