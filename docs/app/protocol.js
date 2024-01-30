2062a5c17370251456ef5e66eaf2d18fef82692d
YeonV <YeonV@users.noreply.github.com>
2024-01-30 23:43:59 +0000

    deploy: ced75723f71fc25ed6cd87f263975151d4e1ecd4

--- docs/app/protocol.js
+++ docs/app/protocol.js
@@ -1,11 +1,11 @@
-const { app } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
 const setupProtocol = () => {
-if (require('electron-squirrel-startup')) {
+  if (require('electron-squirrel-startup')) {
     app.quit()
-    }
-if (process.defaultApp) {
+  }
+  if (process.defaultApp) {
     if (process.argv.length >= 2) {
       app.setAsDefaultProtocolClient('ledfx', process.execPath, [
         path.resolve(process.argv[1])
@@ -16,28 +16,30 @@ if (process.defaultApp) {
   }
 }
 
-const handleProtocol = (wind, gotTheLock, ready) => {
-    if (process.platform === 'win32') {
-        if (!gotTheLock) {
-          app.quit()
-        } else {
-          app.on('second-instance', (event, commandLine, workingDirectory) => {
-            if (wind) {
-              if (wind.isMinimized()) wind.restore()
-              wind.focus()
-              wind.webContents.send('fromMain', [
-                'protocol',
-                JSON.stringify({ event, commandLine, workingDirectory })
-              ])
-            }
-          })
-          ready()
-          app.on('open-url', (event, url) => console.log(event, url))
+const handleProtocol = (getWind, gotTheLock, ready) => {
+  if (process.platform === 'win32') {
+    if (!gotTheLock) {
+      app.quit()
+    } else {
+      app.on('second-instance', (event, commandLine, workingDirectory) => {
+        const wind = getWind()
+        console.log(commandLine, wind)
+        if (wind) {
+          if (wind.isMinimized()) wind.restore()
+          wind.webContents.send('fromMain', [
+            'protocol',
+            JSON.stringify({ event, commandLine, workingDirectory })
+          ])
+          wind.focus()
         }
-      } else {
-        ready()
-        app.on('open-url', (event, url) => console.log(event, url))
-      }
+      })
+      ready()
+      app.on('open-url', (event, url) => console.log(event, url))
+    }
+  } else {
+    ready()
+    app.on('open-url', (event, url) => console.log(event, url))
+  }
 }
 
 module.exports = { setupProtocol, handleProtocol }
