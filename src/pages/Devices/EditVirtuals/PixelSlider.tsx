7df15f30388aa007987931307056292c9a1aa6b3
YeonV <trusted.yeon@gmail.com>
2024-01-31 01:46:13 +0100

    Add number fields for segments inside EditVirtuals

--- src/pages/Devices/EditVirtuals/PixelSlider.tsx
+++ src/pages/Devices/EditVirtuals/PixelSlider.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useThrottledCallback } from 'use-debounce'
 import Slider from '@mui/material/Slider'
+import { Stack, TextField } from '@mui/material'
 import useStore from '../../../store/useStore'
 
 const PixelSlider = ({ s, handleRangeSegment }: any) => {
@@ -39,19 +40,92 @@ const PixelSlider = ({ s, handleRangeSegment }: any) => {
   ]
 
   return (
-    <Slider
-      value={range}
-      marks={marks}
-      valueLabelFormat={(e) => e + 1}
-      min={0}
-      max={devices[s[0]].config.pixel_count - 1}
-      onChange={(_event: any, n: any) => {
-        setRange(n)
-        throttled(_event, n)
-      }}
-      aria-labelledby="range-slider"
-      valueLabelDisplay="auto"
-    />
+    <Stack direction="row" spacing={5} alignItems="flex-start" flexBasis="100%">
+      <Stack
+        direction="row"
+        spacing={1}
+        alignItems="flex-start"
+        alignSelf="flex-end"
+      >
+        <TextField
+          size="small"
+          type="number"
+          sx={{ minWidth: '80px' }}
+          InputProps={{
+            inputProps: {
+              style: { padding: '4.5px 14px' },
+              min: 1,
+              max: devices[s[0]].config.pixel_count
+            }
+          }}
+          value={range[0] + 1}
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+          ) => {
+            if (parseInt(e.target.value, 10) > range[1] + 1) {
+              return
+            }
+            if (parseInt(e.target.value, 10) < 1) {
+              setRange([0, range[1]])
+              throttled(e, [0, range[1]])
+              return
+            }
+            setRange([parseInt(e.target.value, 10) - 1, range[1]])
+            throttled(e, [parseInt(e.target.value, 10) - 1, range[1]])
+          }}
+        />
+        <TextField
+          size="small"
+          type="number"
+          sx={{ minWidth: '80px' }}
+          InputProps={{
+            inputProps: {
+              style: { padding: '4.5px 14px' },
+              min: range[0] + 1,
+              max: devices[s[0]].config.pixel_count
+            }
+          }}
+          value={range[1] + 1}
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+          ) => {
+            if (e.target.value === '') {
+              setRange([range[0], range[0] + 1])
+              throttled(e, [range[0], range[0] + 1])
+              return
+            }
+            if (parseInt(e.target.value, 10) < range[0] + 1) {
+              setRange([range[0], range[0] + 1])
+              throttled(e, [range[0], range[0] + 1])
+              return
+            }
+            if (
+              parseInt(e.target.value, 10) > devices[s[0]].config.pixel_count
+            ) {
+              setRange([range[0], devices[s[0]].config.pixel_count - 1])
+              throttled(e, [range[0], devices[s[0]].config.pixel_count - 1])
+              return
+            }
+            setRange([range[0], parseInt(e.target.value, 10) - 1])
+            throttled(e, [range[0], parseInt(e.target.value, 10) - 1])
+          }}
+        />
+      </Stack>
+      <Slider
+        sx={{ alignSelf: 'center' }}
+        value={range}
+        marks={marks}
+        valueLabelFormat={(e) => e + 1}
+        min={0}
+        max={devices[s[0]].config.pixel_count - 1}
+        onChange={(_event: any, n: any) => {
+          setRange(n)
+          throttled(_event, n)
+        }}
+        aria-labelledby="range-slider"
+        valueLabelDisplay="auto"
+      />
+    </Stack>
   )
 }
 
