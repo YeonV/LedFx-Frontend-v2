65f5c96254ab1101059747cd8860bd894294ecee
YeonV <trusted.yeon@gmail.com>
2024-11-03 19:00:00 +0100

    Fix Number Inputs

--- src/pages/Devices/EditVirtuals/EditMatrix/MSlider.tsx
+++ src/pages/Devices/EditVirtuals/EditMatrix/MSlider.tsx
@@ -1,5 +1,6 @@
 import { Slider, Stack, TextField } from '@mui/material'
 import BladeFrame from '../../../../components/SchemaForm/components/BladeFrame'
+import Number from '../../../../components/Number'
 
 const MSlider = ({
   group,
@@ -25,22 +26,22 @@ const MSlider = ({
           marks={[
             { value: 0, label: '0' },
             {
-              value: devices[currentDevice].config.pixel_count,
-              label: devices[currentDevice].config.pixel_count
+              value: devices[currentDevice].config.pixel_count - 1,
+              label: devices[currentDevice].config.pixel_count - 1
             }
           ]}
           valueLabelDisplay="auto"
           min={0}
-          max={devices[currentDevice].config.pixel_count}
+          max={devices[currentDevice].config.pixel_count - 1}
           value={selectedPixel}
           onChange={handleSliderChange}
         />
         {
           typeof selectedPixel === 'number' 
-          ? <TextField type='number' variant='outlined' value={selectedPixel} onChange={(e) => handleSliderChange(e, parseInt(e.target.value))} />
+          ? <Number min={0} max={devices[currentDevice].config.pixel_count - 1} value={selectedPixel} onChange={(e: any) => handleSliderChange(e, parseInt(e.target.value))} />
           : <Stack direction={'row'} spacing={2}>
-              <TextField type='number' variant='outlined' value={selectedPixel[0]} onBlur={(e) => handleSliderChange(e,[parseInt(e.target.value), selectedPixel[1]], 0)} />
-              <TextField type='number' variant='outlined' value={selectedPixel[1]} onBlur={(e) => handleSliderChange(e,[selectedPixel[0], parseInt(e.target.value)], 1)} />
+              <Number min={0} max={devices[currentDevice].config.pixel_count - 1} value={selectedPixel[0]} onChange={(e) => handleSliderChange(e,[parseInt(e.target.value), selectedPixel[1]], 0)} />
+              <Number min={0} max={devices[currentDevice].config.pixel_count - 1} value={selectedPixel[1]} onChange={(e) => handleSliderChange(e,[selectedPixel[0], parseInt(e.target.value)], 1)} />
             </Stack>          
         }
       </Stack>
