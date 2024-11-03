f704ff3ccbc6a155762bca4b78dda850275b6d72
YeonV <trusted.yeon@gmail.com>
2024-11-03 19:02:32 +0100

    Fix 2d DnD sizing and scrolling

--- src/pages/Devices/EditVirtuals/EditMatrix/MWrapper.tsx
+++ src/pages/Devices/EditVirtuals/EditMatrix/MWrapper.tsx
@@ -12,9 +12,12 @@ const MWrapper = ({ children, move }: any) => {
             ? 'row'
             : 'column',
         maxHeight: 'calc(100vh - 64px)',
+        height: '100%',
         '& .react-transform-wrapper': {
           // flexGrow: 1,
-          overflow: move ? 'auto' : 'hidden'
+          overflow: move ? 'auto' : 'hidden',
+          height: '100%',
+          width: '100%',
         }
       }}
     >
