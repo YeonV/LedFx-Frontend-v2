7a1f09f5c3d7001e05472c4e9a80dff1ae7354e1
YeonV <trusted.yeon@gmail.com>
2024-11-14 00:45:44 +0100

    Better default settings for blender

--- src/pages/Device/Device.tsx
+++ src/pages/Device/Device.tsx
@@ -9,6 +9,7 @@ import MelbankCard from './Frequencies'
 import StreamToCard from './StreamTo'
 import EffectsComplex from './EffectsComplex'
 import { Virtual } from '../../store/api/storeVirtuals'
+import { defaultEffects } from '../Home/BladeScene'
 
 const Device = () => {
   const navigate = useNavigate()
@@ -85,91 +86,27 @@ const Device = () => {
   const setEffects = async () => {
     if (!virtId) return;
     const promises = [];
+    const is2d = virtuals[virtId].config.rows > 1;
     promises.push(setEffect(
       `${virtId}-mask`,
-      'texter2d',
-      {
-        "text": "LedFx",
-        "speed_option_1": 0,
-        "dump": false,
-        "background_color": "#000000",
-        "font": "Blade-5x8",
-        "flip": false,
-        "option_2": false,
-        "option_1": false,
-        "multiplier": 1,
-        "flip_vertical": false,
-        "use_gradient": false,
-        "brightness": 1,
-        "value_option_1": 0.5,
-        "text_color": "#ffffff",
-        "advanced": false,
-        "gradient_roll": 0,
-        "gradient": "linear-gradient(90deg, rgb(255, 0, 0) 0%, rgb(255, 120, 0) 14%, rgb(255, 200, 0) 28%, rgb(0, 255, 0) 42%, rgb(0, 199, 140) 56%, rgb(0, 0, 255) 70%, rgb(128, 0, 128) 84%, rgb(255, 0, 178) 98%)",
-        "background_brightness": 1,
-        "blur": 0,
-        "flip_horizontal": false,
-        "resize_method": "Fast",
-        "diag": false,
-        "height_percent": 100,
-        "text_effect": "Side Scroll",
-        "mirror": false,
-        "test": false,
-        "impulse_decay": 0.1,
-        "rotate": 0,
-        "deep_diag": false,
-        "alpha": false
-      },
+      is2d ? defaultEffects.blenderMask2d.type : defaultEffects.blenderMask.type,
+      is2d ? defaultEffects.blenderMask2d.config : defaultEffects.blenderMask.config,
       true
     ).then(() => {
       updateVirtual(`${virtId}-mask`, true)
     }))
     promises.push(setEffect(
       `${virtId}-foreground`,
-      'blade_power_plus',
-      {
-        "blur": 2,
-        "background_color": "#000080",
-        "multiplier": 0.5,
-        "decay": 0.7,
-        "frequency_range": "Lows (beat+bass)",
-        "gradient": "linear-gradient(90deg, #00ffff 0.00%,#0000ff 100.00%)",
-        "mirror": false,
-        "flip": true,
-        "gradient_roll": 0,
-        "fix_hues": true,
-        "brightness": 1,
-        "background_brightness": 0.63
-      },
+      is2d ? defaultEffects.blenderForeground2d.type : defaultEffects.blenderForeground.type,
+      is2d ? defaultEffects.blenderForeground2d.config : defaultEffects.blenderForeground.config,
       true
     ).then(() => {
       updateVirtual(`${virtId}-foreground`, true)
     }))
     promises.push(setEffect(
       `${virtId}-background`,
-      'plasma2d',
-      {
-        "advanced": false,
-        "background_brightness": 1,
-        "background_color": "#000000",
-        "blur": 0,
-        "brightness": 0.03,
-        "density": 0.5,
-        "density_vertical": 0.1,
-        "diag": false,
-        "dump": false,
-        "flip": false,
-        "flip_horizontal": false,
-        "flip_vertical": false,
-        "frequency_range": "Lows (beat+bass)",
-        "gradient": "linear-gradient(90deg, #ff00b2 0.00%,#ff2800 50.00%,#ffc800 100.00%)",
-        "gradient_roll": 0,
-        "lower": 0.01,
-        "mirror": false,
-        "radius": 0.2,
-        "rotate": 0,
-        "test": false,
-      },
+      is2d ? defaultEffects.blenderBackground2d.type : defaultEffects.blenderBackground.type,
+      is2d ? defaultEffects.blenderBackground2d.config : defaultEffects.blenderBackground.config,
       true
     ).then(() => {
       updateVirtual(`${virtId}-background`, true)
