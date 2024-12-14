b79feaa2d02e85dd730780863e91451380de0d1d
YeonV <trusted.yeon@gmail.com>
2024-12-14 15:13:18 +0100

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,14 +7,14 @@ const config: StorybookConfig = {
       '../src/components/Icons/BladeIcon/BladeIcon.stories.tsx',
       '../src/components/Doc/Api.stories.tsx',
       '../src/components/Popover/Popover.stories.tsx',
-      // '../src/pages/Devices/DeviceCard/DeviceCard.stories.tsx',
+      '../src/pages/Devices/DeviceCard/DeviceCard.stories.tsx',
     ],
   addons: [
     '@storybook/preset-create-react-app',
-    '@storybook/addon-onboarding',
+    // '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+    // '@chromatic-com/storybook',
+    // '@storybook/addon-interactions',
     '@storybook/addon-themes'
   ],
   framework: {
