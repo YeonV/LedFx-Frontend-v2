d29175cd368541bba8d087b1350fa23c3bc09d36
YeonV <trusted.yeon@gmail.com>
2024-12-14 04:51:23 +0100

    BBoolean

---
+++ src/components/SchemaForm/components/Boolean/BBolean.tsx
@@ -0,0 +1,75 @@
+import { Switch, Checkbox, Button, Typography } from '@mui/material'
+import BladeFrame from '../BladeFrame'
+import { BBooleanProps } from './BBoolean.props'
+
+/**
+ * ## Boolean
+ * ### render as `switch`,`checkbox` or `button`
+ */
+const BBoolean = ({
+  index,
+  required,
+  style,
+  type = 'switch',
+  onChange,
+  defaultValue,
+  value,
+  title = '',
+  description = '',
+  hideDesc = false,
+}: BBooleanProps) => {
+  switch (type) {
+    case 'switch':
+      return (
+        <BladeFrame
+          index={index}
+          required={required}
+          style={style}
+          title={title}
+        >
+          <Switch
+            defaultValue={defaultValue || value}
+            checked={value}
+            onChange={onChange}
+            name={title}
+            color="primary"
+          />
+          {!hideDesc && description ? (
+            <Typography variant="body2" className="MuiFormHelperText-root">
+              {description}{' '}
+            </Typography>
+          ) : null}
+        </BladeFrame>
+      )
+    case 'checkbox':
+      return (
+        <BladeFrame
+          index={index}
+          title={title}
+        >
+          <Checkbox
+            defaultValue={defaultValue || value}
+            checked={value}
+            onChange={onChange}
+            name={title}
+            color="primary"
+          />
+        </BladeFrame>
+      )
+    case 'button':
+      return (
+        <Button
+          color="primary"
+          variant={value ? 'contained' : 'outlined'}
+          onClick={onChange}
+        >
+          {title}
+        </Button>
+      )
+
+    default:
+      return <div>unset</div>
+  }
+}
+
+export default BBoolean
---
+++ src/components/SchemaForm/components/Boolean/BBoolean.props.tsx
@@ -0,0 +1,12 @@
+export interface BBooleanProps {
+  index?: number
+  required?: boolean
+  style?: any
+  type?: 'switch' | 'checkbox' | 'button'
+  onChange: (e: any) => void
+  defaultValue?: any
+  value: boolean
+  title?: string
+  description?: string
+  hideDesc?: boolean
+}
