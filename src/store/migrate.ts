e0fa585551f9f3337e6bee787c143418bcde1680
YeonV <trusted.yeon@gmail.com>
2025-01-19 15:47:11 +0100

    Config Migrator

---
+++ src/store/migrate.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export interface MigrationState {
+  [key: string]: any
+}
+
+interface Migrations {
+  [version: number]: (state: MigrationState) => MigrationState
+}
+
+export const migrations: Migrations = {
+  // Removes an existing function (deprecatedFunction)
+  13: (state) => {
+    const { deprecatedFunction, ...rest } = state
+    return {
+      ...rest
+      // Remove a deprecated function
+    }
+  },
+
+  // Removes an existing value (deprecatedValue) and repositions an existing value (existingValue) based on another value (anotherValue)
+  14: (state) => {
+    const { deprecatedValue, ...rest } = state
+    return {
+      ...rest,
+      existingValue: state.anotherValue + 1 // Change position of an existing value
+      // Remove a deprecated value
+    }
+  },
+
+  // Adds a new value (newValue) based on an existing value (existingValue). Also adds a new function (newFunction)
+  15: (state) => ({
+    ...state,
+    // Transformations for version 15
+    newValue: state.existingValue + 1, // Add new value based on existing value
+    // Add a new function
+    newFunction: () => console.log('New Function')
+  }),
+
+  16: (state) => ({ ...state }),
+  17: (state) => ({
+    ...state
+    // Add new state properties or transform existing ones
+  })
+}
