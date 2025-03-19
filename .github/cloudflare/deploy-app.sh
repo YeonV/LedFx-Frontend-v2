f3e1e19c1ef580c6e95db829db09b8a5d34651f7
YeonV <trusted.yeon@gmail.com>
2025-03-19 05:19:45 +0100

    optimize cloudflare

---
+++ .github/cloudflare/deploy-app.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+cd ../../ # Navigate to the root directory
+
+# Define the file pattern to check (for ledfx.stream)
+FILE_PATTERN="!./storybook/* !./src/stories/* !**/*.stories.tsx !./.github/workflows/* !./.vscode/* !./README.md ./* **/*"
+
+# Check if files matching the pattern have changed
+if git diff --quiet HEAD~1 HEAD -- $FILE_PATTERN; then
+  echo "No relevant ledfx.stream app file changes detected. Skipping build."
+  exit 0
+else
+  echo "Relevant ledfx.stream app file changes detected. Proceeding with build."
+  yarn buildcloudflare
+fi
---
+++ .github/cloudflare/deploy-doc.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+cd ../../ # Navigate to the root directory
+
+# Define the file pattern to check (for docs.ledfx.stream)
+FILE_PATTERN="./storybook/* ./src/stories/* **/*.stories.tsx"
+
+# Check if files matching the pattern have changed
+if git diff --quiet HEAD~1 HEAD -- $FILE_PATTERN; then
+  echo "No relevant docs.ledfx.stream documentation file changes detected. Skipping build."
+  exit 0
+else
+  echo "Relevant docs.ledfx.stream documentation file changes detected. Proceeding with build."
+  yarn build-storybook
+fi
