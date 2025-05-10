c0f1d90f1c0d4a43e02c0b51d19d22db242c5fce
YeonV <trusted.yeon@gmail.com>
2025-05-11 01:20:15 +0200

    implement new VirtualPicker

--- src/components/SchemaForm/EffectsSchemaForm/EffectSchemaForm.tsx
+++ src/components/SchemaForm/EffectsSchemaForm/EffectSchemaForm.tsx
@@ -4,9 +4,8 @@ import BladeSelect from '../components/String/BladeSelect'
 import BladeSlider from '../components/Number/BladeSlider'
 import GradientPickerWrapper from '../components/GradientPicker/GradientPicker.wrapper'
 import { EffectSchemaFormProps } from './EffectSchemaForm.props'
-import { MenuItem, Select } from '@mui/material'
 import useStore from '../../../store/useStore'
-import BladeFrame from '../components/BladeFrame'
+import VirtualPicker from '../components/VirtualPicker/VirtualPicker'
 
 const PREFIX = 'EffectSchemaForm'
 
@@ -33,7 +32,6 @@ const EffectSchemaForm = ({
   descriptions,
   selectedType
 }: EffectSchemaFormProps) => {
-  const virtuals = useStore((state) => state.virtuals)
   const blenderAutomagic = useStore((state) => state.uiPersist.blenderAutomagic)
   return (
     <Root className={classes.bladeSchemaForm}>
@@ -61,51 +59,15 @@ const EffectSchemaForm = ({
               {
                 const complex = ['mask', 'foreground', 'background']
                 if (selectedType === 'blender' && complex.includes(s.id))
-                  if (!blenderAutomagic) {
-                    return (
-                      <BladeFrame
-                        title={s.title}
-                        key={s.id}
-                        required
-                        style={{
-                          margin: '0.5rem 0',
-                          flexBasis: '49%',
-                          width: 'unset'
-                        }}
-                      >
-                        <Select
-                          onChange={(e: any) => {
-                            const c: Record<string, unknown> = {}
-                            c[s.id] = e.target.value
-                            return handleEffectConfig && handleEffectConfig(c)
-                          }}
-                          value={model[s.id]}
-                          fullWidth
-                          disableUnderline
-                        >
-                          {Object.keys(virtuals)
-                            .filter(
-                              (v) =>
-                                typeof virtuals[v].is_device === 'string' &&
-                                virtuals[v].is_device !== '' &&
-                                v
-                            )
-                            .map((v) => {
-                              return (
-                                <MenuItem
-                                  key={virtuals[v].id}
-                                  value={virtuals[v].id}
-                                >
-                                  {virtuals[v].config.name}
-                                </MenuItem>
-                              )
-                            })}
-                        </Select>
-                      </BladeFrame>
-                    )
-                  } else {
-                    return null
-                  }
+                  return (
+                    <VirtualPicker
+                      title={s.title}
+                      id={s.id}
+                      model={model}
+                      hide={blenderAutomagic}
+                      handleEffectConfig={handleEffectConfig}
+                    />
+                  )
               }
               return (
                 <BladeSelect
@@ -167,11 +129,19 @@ const EffectSchemaForm = ({
                   index={i}
                   title={s.id}
                   hideDesc={descriptions === 'Hide'}
-                  // selectedType={selectedType}
-                  // model={model}
                   virtId={virtId}
                   wrapperStyle={{ width: '49%' }}
                   isGradient={s.gradient}
+                />
+              )
+            case 'virtual':
+              return (
+                <VirtualPicker
+                  title={s.title}
+                  id={s.id}
+                  model={model}
+                  hide={blenderAutomagic}
+                  handleEffectConfig={handleEffectConfig}
                 />
               )
             default:
