b63eab385ea44d5bcde8a3c0d3362b3fb378b8ac
Miniontoby <tobias.gaarenstroom@gmail.com>
2025-07-02 15:16:11 +0200

    Added Launchkey support
    Added Launchkey MK3 and MiniMK3 support
    
    > ![NOTE]
    > Launchkey MK3 hasn't been tested, but should work

---
+++ src/utils/MidiDevices/LaunchkeyDevice.ts
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+import { lpColors } from './LaunchpadX/lpColors'
+
+export interface LaunchkeyMK3Device {
+  buttonNumbers: number[][]
+  colors: Record<string, number>
+  commonColors: Record<string, number>
+  globalColors: {
+    sceneActiveType: string
+    sceneActiveColor: string
+    sceneInactiveType: string
+    sceneInactiveColor: string
+    commandType: string
+    commandColor: string
+  }
+  command: {
+    standalone: number[]
+    daw: number[]
+    ledOn: (number | string)[]
+    ledFlash: (number | string)[]
+    ledPulse: (number | string)[]
+    text: (number | string)[]
+    stopText: number[]
+  }
+  fn: {
+    ledOff: (buttonNumber: number) => number[]
+    ledOn: (
+      buttonNumber: number,
+      color: keyof typeof lpColors | number,
+      mode?: 'solid' | 'flash' | 'pulse'
+    ) => number[]
+    ledSolid: (buttonNumber: number, color: keyof typeof lpColors | number) => number[]
+    ledFlash: (buttonNumber: number, color: keyof typeof lpColors | number) => number[]
+    ledPulse: (buttonNumber: number, color: keyof typeof lpColors | number) => number[]
+    text: (text: string) => number[]
+  }
+}
+
+export interface LaunchkeyMiniMK3Device {
+  buttonNumbers: number[][]
+  colors: Record<string, number>
+  commonColors: Record<string, number>
+  globalColors: {
+    sceneActiveType: string
+    sceneActiveColor: string
+    sceneInactiveType: string
+    sceneInactiveColor: string
+    commandType: string
+    commandColor: string
+  }
+  command: {
+    standalone: number[]
+    daw: number[]
+    ledOn: (number | string)[]
+    ledFlash: (number | string)[]
+    ledPulse: (number | string)[]
+  }
+  fn: {
+    ledOff: (buttonNumber: number) => number[]
+    ledOn: (
+      buttonNumber: number,
+      color: keyof typeof lpColors | number,
+      mode?: 'solid' | 'flash' | 'pulse'
+    ) => number[]
+    ledSolid: (buttonNumber: number, color: keyof typeof lpColors | number) => number[]
+    ledFlash: (buttonNumber: number, color: keyof typeof lpColors | number) => number[]
+    ledPulse: (buttonNumber: number, color: keyof typeof lpColors | number) => number[]
+  }
+}
+
+export type LaunchkeyDevice = LaunchkeyMK3Device | LaunchkeyMiniMK3Device;
---
+++ src/utils/MidiDevices/LaunchkeyMiniMK3/LaunchkeyMiniMK3.ts
@@ -0,0 +1,53 @@
+import { LaunchkeyMiniMK3Device } from '../LaunchkeyDevice'
+import { lpColors, lpCommonColors } from '../LaunchpadX/lpColors'
+
+export const LaunchkeyMiniMK3: LaunchkeyMiniMK3Device = {
+  buttonNumbers: [
+    [40, 41, 42, 43, 48, 49, 50, 51],
+    [36, 37, 38, 39, 44, 45, 46, 47],
+  ],
+  colors: lpColors,
+  commonColors: lpCommonColors,
+  globalColors: {
+    sceneActiveType: 'rgb',
+    sceneActiveColor: 'rgb(0, 255, 0)',
+    sceneInactiveType: 'rgb',
+    sceneInactiveColor: 'rgb(255, 0, 0)',
+    commandType: 'rgb',
+    commandColor: 'rgb(255, 255, 0)'
+  },
+  command: {
+    standalone: [0x9f, 0x0c, 0x00],
+    daw: [0x9f, 0x0c, 0xf7],
+    ledOn: [0x99, 'buttonNumber', 'color'],
+    ledFlash: [0x9a, 'buttonNumber', 'color'],
+    ledPulse: [0x9b, 'buttonNumber', 'color'],
+  },
+  fn: {
+    ledOff: (buttonNumber: number) => [0x99, buttonNumber, 0x00],
+    ledOn: (
+      buttonNumber: number,
+      color: keyof typeof lpColors | number,
+      mode: 'solid' | 'flash' | 'pulse' = 'solid'
+    ) => [
+      mode === 'pulse' ? 0x9b : mode === 'flash' ? 0x9a : 0x99,
+      buttonNumber,
+      typeof color === 'number' ? color : lpColors[color]
+    ],
+    ledSolid: (buttonNumber: number, color: keyof typeof lpColors | number) => [
+      0x99,
+      buttonNumber,
+      typeof color === 'number' ? color : lpColors[color]
+    ],
+    ledFlash: (buttonNumber: number, color: keyof typeof lpColors | number) => [
+      0x9a,
+      buttonNumber,
+      typeof color === 'number' ? color : lpColors[color]
+    ],
+    ledPulse: (buttonNumber: number, color: keyof typeof lpColors | number) => [
+      0x9b,
+      buttonNumber,
+      typeof color === 'number' ? color : lpColors[color]
+    ],
+  }
+}
