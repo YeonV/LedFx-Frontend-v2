f926d9e71175636ef80e00efaf3dae4403238e20
Miniontoby <tobias.gaarenstroom@gmail.com>
2025-07-02 15:16:11 +0200

    Added Launchkey support
    Added Launchkey MK3 and MiniMK3 support
    
    > ![NOTE]
    > Launchkey MK3 hasn't been tested, but should work

--- src/utils/MidiDevices/MidiDevices.ts
+++ src/utils/MidiDevices/MidiDevices.ts
@@ -1,6 +1,8 @@
 import { LaunchpadMK2 } from './LaunchpadMK2/LaunchpadMK2'
 import { LaunchpadS } from './LaunchpadS/LaunchpadS'
 import { LaunchpadX } from './LaunchpadX/LaunchpadX'
+import { LaunchkeyMK3 } from './LaunchkeyMK3/LaunchkeyMK3'
+import { LaunchkeyMiniMK3 } from './LaunchkeyMiniMK3/LaunchkeyMiniMK3'
 
 export const Launchpad = {
   X: LaunchpadX,
@@ -8,6 +10,12 @@ export const Launchpad = {
   S: LaunchpadS
 }
 
+export const Launchkey = {
+  MK3: LaunchkeyMK3,
+  MiniMK3: LaunchkeyMiniMK3
+}
+
 export const MidiDevices = {
-  Launchpad
+  Launchpad,
+  Launchkey
 }
