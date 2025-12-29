c0f64360efa160e1df1b84627a99828098b5ef3c
bigredfrog <anthony.tod@gmail.com>
2025-12-28 22:26:25 -0500

    default matrix to on in devices (#80)

--- src/store/ui/storeGeneral.tsx
+++ src/store/ui/storeGeneral.tsx
@@ -126,7 +126,7 @@ const storeGeneral = (set: any) => ({
       'general/toggleGraphsMulti'
     )
   },
-  showMatrix: false,
+  showMatrix: true,
   toggleShowMatrix: () => {
     set(
       produce((state: IStore) => {
