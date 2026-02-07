aeed728bf6f992007521750f44f80fddf4f88d2b
YeonV <trusted.yeon@gmail.com>
2026-02-08 00:59:22 +0100

    feat: update PixelSlider to display LED count and improve layout

--- src/pages/Devices/EditVirtuals/PixelSlider.tsx
+++ src/pages/Devices/EditVirtuals/PixelSlider.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useThrottledCallback } from 'use-debounce'
 import Slider from '@mui/material/Slider'
-import { Stack, TextField } from '@mui/material'
+import { Stack, TextField, Typography } from '@mui/material'
 import useStore from '../../../store/useStore'
 
 const PixelSlider = ({ s, handleRangeSegment }: any) => {
@@ -37,64 +37,69 @@ const PixelSlider = ({ s, handleRangeSegment }: any) => {
 
   return (
     <Stack direction="row" spacing={5} alignItems="flex-start" flexBasis="100%">
-      <Stack direction="row" spacing={1} alignItems="flex-start" alignSelf="flex-end">
-        <TextField
-          size="small"
-          type="number"
-          sx={{ minWidth: '80px' }}
-          slotProps={{
-            htmlInput: {
-              style: { padding: '4.5px 14px' },
-              min: 1,
-              max: devices[s[0]].config.pixel_count
-            }
-          }}
-          value={range[0] + 1}
-          onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-            if (parseInt(e.target.value, 10) > range[1] + 1) {
-              return
-            }
-            if (parseInt(e.target.value, 10) < 1) {
-              setRange([0, range[1]])
-              throttled(e, [0, range[1]])
-              return
-            }
-            setRange([parseInt(e.target.value, 10) - 1, range[1]])
-            throttled(e, [parseInt(e.target.value, 10) - 1, range[1]])
-          }}
-        />
-        <TextField
-          size="small"
-          type="number"
-          sx={{ minWidth: '80px' }}
-          slotProps={{
-            htmlInput: {
-              style: { padding: '4.5px 14px' },
-              min: range[0] + 1,
-              max: devices[s[0]].config.pixel_count
-            }
-          }}
-          value={range[1] + 1}
-          onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-            if (e.target.value === '') {
-              setRange([range[0], range[0] + 1])
-              throttled(e, [range[0], range[0] + 1])
-              return
-            }
-            if (parseInt(e.target.value, 10) < range[0] + 1) {
-              setRange([range[0], range[0] + 1])
-              throttled(e, [range[0], range[0] + 1])
-              return
-            }
-            if (parseInt(e.target.value, 10) > (devices[s[0]].config.pixel_count || 0)) {
-              setRange([range[0], (devices[s[0]].config.pixel_count || 0) - 1])
-              throttled(e, [range[0], (devices[s[0]].config.pixel_count || 0) - 1])
-              return
-            }
-            setRange([range[0], parseInt(e.target.value, 10) - 1])
-            throttled(e, [range[0], parseInt(e.target.value, 10) - 1])
-          }}
-        />
+      <Stack direction="column" spacing={1} alignItems="flex-start">
+        <Typography color="textSecondary" marginTop={1} marginBottom={-1}>
+          {pixelRange[1] - pixelRange[0]} LEDs
+        </Typography>
+        <Stack direction="row" spacing={1} alignItems="flex-start" alignSelf="flex-end">
+          <TextField
+            size="small"
+            type="number"
+            sx={{ minWidth: '80px' }}
+            slotProps={{
+              htmlInput: {
+                style: { padding: '4.5px 14px' },
+                min: 1,
+                max: devices[s[0]].config.pixel_count
+              }
+            }}
+            value={range[0] + 1}
+            onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+              if (parseInt(e.target.value, 10) > range[1] + 1) {
+                return
+              }
+              if (parseInt(e.target.value, 10) < 1) {
+                setRange([0, range[1]])
+                throttled(e, [0, range[1]])
+                return
+              }
+              setRange([parseInt(e.target.value, 10) - 1, range[1]])
+              throttled(e, [parseInt(e.target.value, 10) - 1, range[1]])
+            }}
+          />
+          <TextField
+            size="small"
+            type="number"
+            sx={{ minWidth: '80px' }}
+            slotProps={{
+              htmlInput: {
+                style: { padding: '4.5px 14px' },
+                min: range[0] + 1,
+                max: devices[s[0]].config.pixel_count
+              }
+            }}
+            value={range[1] + 1}
+            onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+              if (e.target.value === '') {
+                setRange([range[0], range[0] + 1])
+                throttled(e, [range[0], range[0] + 1])
+                return
+              }
+              if (parseInt(e.target.value, 10) < range[0] + 1) {
+                setRange([range[0], range[0] + 1])
+                throttled(e, [range[0], range[0] + 1])
+                return
+              }
+              if (parseInt(e.target.value, 10) > (devices[s[0]].config.pixel_count || 0)) {
+                setRange([range[0], (devices[s[0]].config.pixel_count || 0) - 1])
+                throttled(e, [range[0], (devices[s[0]].config.pixel_count || 0) - 1])
+                return
+              }
+              setRange([range[0], parseInt(e.target.value, 10) - 1])
+              throttled(e, [range[0], parseInt(e.target.value, 10) - 1])
+            }}
+          />
+        </Stack>
       </Stack>
       <Slider
         sx={{ alignSelf: 'center' }}
