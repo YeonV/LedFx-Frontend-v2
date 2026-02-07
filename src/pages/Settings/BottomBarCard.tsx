3bfd38851318cd352732f338f20b5cf73a9f4f9e
YeonV <trusted.yeon@gmail.com>
2026-02-08 00:59:29 +0100

    fix: change Audio Visualiser setting row from alpha to beta

--- src/pages/Settings/BottomBarCard.tsx
+++ src/pages/Settings/BottomBarCard.tsx
@@ -38,7 +38,7 @@ const BottomBarCard = () => {
         onChange={() => setFeatures('showMidiInBottomBar', !features.showMidiInBottomBar)}
       />
       <SettingsRow
-        alpha
+        beta
         title="Audio Visualiser"
         checked={features.showVisualiserInBottomBar}
         onChange={() =>
